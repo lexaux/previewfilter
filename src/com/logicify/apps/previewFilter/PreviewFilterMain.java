d266085d96949228c42ff8adc9a7387abb1c54bd
Alexander Cherednichenko <lexaux@gmail.com>
2013-10-28 18:34:20 +0200

    Added initial setup for the application.

---
+++ src/com/logicify/apps/previewFilter/PreviewFilterMain.java
@@ -0,0 +1,17 @@
+package com.logicify.apps.previewFilter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PreviewFilterMain extends Activity
+{
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

65f6f464bde82a3977f99944983ce019aa3bf7f5
Alexander Cherednichenko <lexaux@gmail.com>
2013-11-02 16:02:02 +0200

    Force switched the activity to the Landscape orientation.

--- src/com/logicify/apps/previewFilter/CameraPreview.java
+++ src/com/logicify/apps/previewFilter/CameraPreview.java
@@ -2,10 +2,12 @@ package com.logicify.apps.previewFilter;
 
 import android.content.Context;
 import android.hardware.Camera;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Camera preview class.
@@ -34,7 +36,6 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         try
         {
             mCamera.setPreviewDisplay(mHolder);
-            mCamera.startPreview();
         } catch (IOException e)
         {
             mCamera.release();
@@ -45,7 +46,41 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height)
     {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
 
+        if (mHolder.getSurface() == null)
+        {
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try
+        {
+            mCamera.stopPreview();
+        } catch (Exception e)
+        {
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+        Camera.Parameters mParams = mCamera.getParameters();
+        List<Camera.Size> previewSizes = mParams.getSupportedPreviewSizes();
+        Camera.Size previewSize = previewSizes.get(previewSizes.size() - 1);
+        mParams.setPreviewSize(previewSize.width, previewSize.height);
+        mCamera.setParameters(mParams);
+
+        // start preview with new settings
+        try
+        {
+            mCamera.startPreview();
+
+        } catch (Exception e)
+        {
+            Log.e(Util.TAG, "Error starting camera preview: " + e.getMessage(), e);
+        }
     }
 
     @Override

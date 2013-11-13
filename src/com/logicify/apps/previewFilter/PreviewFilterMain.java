0a5068b27657d4a6f7045be416047a1e742a57f3
Alexander Cherednichenko <lexaux@gmail.com>
2013-11-13 12:14:26 +0200

    Added simplest-possible implementation of the TextureView and camera preview.

--- src/com/logicify/apps/previewFilter/PreviewFilterMain.java
+++ src/com/logicify/apps/previewFilter/PreviewFilterMain.java
@@ -1,24 +1,62 @@
 package com.logicify.apps.previewFilter;
 
 import android.app.Activity;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.TextureView;
+import android.view.View;
 
-public class PreviewFilterMain extends Activity
+import java.io.IOException;
+
+public class PreviewFilterMain extends Activity implements TextureView.SurfaceTextureListener
 {
     /**
      * Called when the activity is first created.
      */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
+    private Camera mCamera;
+    private TextureView mTextureView;
+
+    protected void onCreate(Bundle savedInstanceState)
     {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-
         super.onCreate(savedInstanceState);
-        setContentView(new CameraPreview(this));
+
+        mTextureView = new TextureView(this);
+        mTextureView.setSurfaceTextureListener(this);
+
+        setContentView(mTextureView);
     }
+
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height)
+    {
+        mCamera = Camera.open();
+//        mTextureView.setVisibility(View.INVISIBLE);
+
+        try
+        {
+            mCamera.setPreviewTexture(surface);
+            mCamera.startPreview();
+        } catch (IOException ioe)
+        {
+            // Something bad happened
+        }
+    }
+
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height)
+    {
+        // Ignored, Camera does all the work for us
+    }
+
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface)
+    {
+        mCamera.stopPreview();
+        mCamera.release();
+        return true;
+    }
+
+    public void onSurfaceTextureUpdated(SurfaceTexture surface)
+    {
+        // Invoked every time there's a new Camera preview frame
+    }
+
 }

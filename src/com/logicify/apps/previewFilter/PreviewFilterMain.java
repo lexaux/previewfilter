aa5d49f5218710796b4c496fb69115cb0ac4c365
Alexander Cherednichenko <lexaux@gmail.com>
2013-11-14 20:52:24 +0200

    Intermediate commit; using textureview as a destination for the image stream from cam.

--- src/com/logicify/apps/previewFilter/PreviewFilterMain.java
+++ src/com/logicify/apps/previewFilter/PreviewFilterMain.java
@@ -1,62 +1,31 @@
 package com.logicify.apps.previewFilter;
 
 import android.app.Activity;
-import android.graphics.SurfaceTexture;
-import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.TextureView;
-import android.view.View;
+import android.widget.FrameLayout;
 
-import java.io.IOException;
-
-public class PreviewFilterMain extends Activity implements TextureView.SurfaceTextureListener
+public class PreviewFilterMain extends Activity
 {
     /**
      * Called when the activity is first created.
      */
-    private Camera mCamera;
     private TextureView mTextureView;
+    private CameraPreview preview;
 
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
 
         mTextureView = new TextureView(this);
-        mTextureView.setSurfaceTextureListener(this);
 
-        setContentView(mTextureView);
+        FrameLayout aLayout = new FrameLayout(this);
+        aLayout.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.FILL_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT));
+        aLayout.addView(mTextureView);
+
+        preview = new CameraPreview(this, mTextureView);
+        aLayout.addView(preview);
+
+        setContentView(aLayout);
     }
-
-    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height)
-    {
-        mCamera = Camera.open();
-//        mTextureView.setVisibility(View.INVISIBLE);
-
-        try
-        {
-            mCamera.setPreviewTexture(surface);
-            mCamera.startPreview();
-        } catch (IOException ioe)
-        {
-            // Something bad happened
-        }
-    }
-
-    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height)
-    {
-        // Ignored, Camera does all the work for us
-    }
-
-    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface)
-    {
-        mCamera.stopPreview();
-        mCamera.release();
-        return true;
-    }
-
-    public void onSurfaceTextureUpdated(SurfaceTexture surface)
-    {
-        // Invoked every time there's a new Camera preview frame
-    }
-
 }
